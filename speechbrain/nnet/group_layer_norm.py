3587d9be11c7c586549a688157f98915b7d5e602
Jianyuan Zhong <jzhong9@u.rochester.edu>
2020-09-14 11:39:14 -0400

    minor changes

--- speechbrain/nnet/group_layer_norm.py
+++ speechbrain/nnet/group_layer_norm.py
@@ -16,14 +16,14 @@ class GroupLayerNorm(nn.Module):
         self.dim = first_input.shape[-1]
 
         # self.weight = nn.Parameter(
-        #     torch.ones([1, 1, self.dim], device=first_input.device)
+        #     torch.ones([self.dim], device=first_input.device)
         # )
         # self.bias = nn.Parameter(
-        #     torch.zeros([1, 1, self.dim], device=first_input.device)
+        #     torch.zeros([self.dim], device=first_input.device)
         # )
 
         # self.norm = nn.LayerNorm(
-        #     self.dim, eps=self.eps, elementwise_affine=False
+        #     self.dim // self.num_rims, eps=self.eps, elementwise_affine=False
         # ).to(first_input.device)
         self.norm = nn.GroupNorm(self.num_rims, self.dim, eps=self.eps).to(
             first_input.device
@@ -33,7 +33,7 @@ class GroupLayerNorm(nn.Module):
         if init_params:
             self.init_params(x)
 
-        bsz, seq_len, _ = x.shape
+        # bsz, seq_len, _ = x.shape
         # x = x.view(bsz, seq_len, self.num_rims, self.dim // self.num_rims)
         x = x.permute(0, 2, 1)
 
